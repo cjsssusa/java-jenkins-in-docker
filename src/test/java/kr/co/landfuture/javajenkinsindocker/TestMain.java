44287cdd896370faf56e3a5d4bc113e55434d80d
cjsssusa <cjsssusa@gmail.com>
2021-10-25 12:36:22 -0400

    Added java demo application files

---
+++ src/test/java/kr/co/landfuture/javajenkinsindocker/TestMain.java
@@ -0,0 +1,13 @@
+package kr.co.landfuture.javajenkinsindocker;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+public class TestMain {
+
+  @Test
+  public void testInputIsEven() {
+    assertTrue(Main.checkIfInputIsAnEvenNumber(22)); // Assertion
+  }
+}
