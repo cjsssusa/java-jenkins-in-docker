57f254e029a477eeae4463c3c69138ba7a880782
cjsssusa <cjsssusa@gmail.com>
2021-10-25 19:46:32 -0400

    a

--- src/test/java/kr/co/landfuture/javajenkinsindocker/TestMain.java
+++ src/test/java/kr/co/landfuture/javajenkinsindocker/TestMain.java
@@ -8,6 +8,6 @@ public class TestMain {
 
   @Test
   public void testInputIsEven() {
-    assertTrue(Main.checkIfInputIsAnEvenNumber(22)); // Assertion
+    assertTrue(Main.checkIfInputIsAnEvenNumber(24)); // Assertion
   }
 }
